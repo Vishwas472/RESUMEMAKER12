c6d5f4f65a5b2916dc628b9fced0b4c32dfa7e22
Vishwas472 <129184702+Vishwas472@users.noreply.github.com>
2023-04-19 09:28:45 +0530

    RESUMEMAKER
    4 PAGE

---
+++ app/src/main/java/com/example/resumemaker12/persnoaldetails.java
@@ -0,0 +1,13 @@
+package com.example.resumemaker12;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class persnoaldetails extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_persnoaldetails);
+    }
+}

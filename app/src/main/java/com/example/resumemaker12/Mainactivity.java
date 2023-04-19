585522baac28e77281970dd112b757de951c7831
Vishwas472 <129184702+Vishwas472@users.noreply.github.com>
2023-04-19 11:51:35 +0530

    java backend code
    java code first page

--- app/src/main/java/com/example/resumemaker12/Mainactivity.java
+++ app/src/main/java/com/example/resumemaker12/Mainactivity.java
@@ -2,13 +2,30 @@ package com.example.resumemaker12;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class Mainactivity extends AppCompatActivity {
+    ImageView imgADD;
 
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_resumemaker);
+        imgADD =findViewById(R.id.imgADD);
+
+        imgADD.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i= new Intent(Mainactivity.this,resumemaker.class);
+                startActivity(i);
+            }
+        });
+
+
     }
 }
